c9b92706ffc6a48782baf17241904129aeed9033
mfnalex <1122571+mfnalex@users.noreply.github.com>
2021-06-24 16:32:54 +0200

    Initial commit

---
+++ src/main/java/de/jeff_media/customblockdata/CustomBlockData.java
@@ -0,0 +1,134 @@
+package de.jeff_media.customblockdata;
+
+import org.bukkit.Chunk;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
+import org.bukkit.persistence.PersistentDataAdapterContext;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Set;
+
+/**
+ * PersistentDataContainer for blocks!
+ * <p>
+ * Blocks do not implement PersistentDataHolder, so you cannot store custom data inside them.
+ * We can however just use the chunk instead. Every Block gets its own PersistentDataContainer,
+ * created and stored only when needed.
+ * <p>
+ * Once you clear the custom data of a block, the PersistentDataContainer also gets removed
+ * from the chunk's PersistentDataContainer.
+ */
+public class CustomBlockData implements PersistentDataContainer {
+
+    private final PersistentDataContainer pdc;
+    private final Chunk chunk;
+    private final NamespacedKey key;
+
+    /**
+     * Gets the PersistentDataContainer associated with the given block and plugin
+     *
+     * @param block  Block
+     * @param plugin Plugin
+     */
+    public CustomBlockData(final @NotNull Block block, final @NotNull Plugin plugin) {
+        this.chunk = block.getChunk();
+        this.pdc = getPersistentDataContainer();
+        this.key = new NamespacedKey(plugin, getKey(block));
+    }
+
+    /**
+     * Gets the PersistentDataContainer associated with the given block and plugin
+     *
+     * @param block     Block
+     * @param namespace Namespace
+     *
+     * @deprecated Use {@link #CustomBlockData(Block, Plugin)} instead.
+     */
+    @Deprecated()
+    public CustomBlockData(final @NotNull Block block, final @NotNull String namespace) {
+        this.chunk = block.getChunk();
+        this.pdc = getPersistentDataContainer();
+        this.key = new NamespacedKey(namespace, getKey(block));
+    }
+
+    @NotNull
+    private static String getKey(@NotNull Block block) {
+        final int x = block.getX() & 0x000F;
+        final int y = block.getY() & 0x00FF;
+        final int z = block.getZ() & 0x000F;
+        return String.format("x%dy%dz%d", x, y, z);
+    }
+
+    @NotNull
+    private PersistentDataContainer getPersistentDataContainer() {
+        final PersistentDataContainer chunkPDC = chunk.getPersistentDataContainer();
+        final PersistentDataContainer blockPDC;
+        if (chunkPDC.has(key, PersistentDataType.TAG_CONTAINER)) {
+            blockPDC = chunkPDC.get(key, PersistentDataType.TAG_CONTAINER);
+            assert blockPDC != null;
+            return blockPDC;
+        }
+        blockPDC = chunkPDC.getAdapterContext().newPersistentDataContainer();
+        chunkPDC.set(key, PersistentDataType.TAG_CONTAINER, blockPDC);
+        return blockPDC;
+    }
+
+    private void save() {
+        if (pdc.isEmpty()) {
+            chunk.getPersistentDataContainer().remove(key);
+        } else {
+            chunk.getPersistentDataContainer().set(key, PersistentDataType.TAG_CONTAINER, pdc);
+        }
+    }
+
+    @Override
+    public <T, Z> void set(@NotNull NamespacedKey namespacedKey, @NotNull PersistentDataType<T, Z> persistentDataType, @NotNull Z z) {
+        pdc.set(namespacedKey, persistentDataType, z);
+        save();
+    }
+
+    @Override
+    public <T, Z> boolean has(@NotNull NamespacedKey namespacedKey, @NotNull PersistentDataType<T, Z> persistentDataType) {
+        return pdc.has(namespacedKey, persistentDataType);
+    }
+
+    @Nullable
+    @Override
+    public <T, Z> Z get(@NotNull NamespacedKey namespacedKey, @NotNull PersistentDataType<T, Z> persistentDataType) {
+        return pdc.get(namespacedKey, persistentDataType);
+    }
+
+    @NotNull
+    @Override
+    public <T, Z> Z getOrDefault(@NotNull NamespacedKey namespacedKey, @NotNull PersistentDataType<T, Z> persistentDataType, @NotNull Z z) {
+        return pdc.getOrDefault(namespacedKey, persistentDataType, z);
+    }
+
+    @NotNull
+    @Override
+    public Set<NamespacedKey> getKeys() {
+        return pdc.getKeys();
+    }
+
+    @Override
+    public void remove(@NotNull NamespacedKey namespacedKey) {
+        pdc.remove(namespacedKey);
+        save();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return pdc.isEmpty();
+    }
+
+    @NotNull
+    @Override
+    public PersistentDataAdapterContext getAdapterContext() {
+        return pdc.getAdapterContext();
+    }
+}
+
