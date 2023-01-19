08232cf85f7879bbdd1e71e0aa6e77383367a5b9
mfnalex <1122571+mfnalex@users.noreply.github.com>
2023-01-19 19:35:55 +0100

    trying to get mockbukkit to work

---
+++ src/test/java/com/jeff_media/customblockdata/TestPlugin.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2023 Alexander Majka (mfnalex) / JEFF Media GbR
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License.
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * If you need help or have any suggestions, feel free to join my Discord and head to #programming-help:
+ *
+ * Discord: https://discord.jeff-media.com/
+ *
+ * If you find this library helpful or if you're using it one of your paid plugins, please consider leaving a donation
+ * to support the further development of this project :)
+ *
+ * Donations: https://paypal.me/mfnalex
+ */
+
+package com.jeff_media.customblockdata;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPluginLoader;
+
+import java.io.File;
+
+public class TestPlugin extends JavaPlugin {
+
+    public TestPlugin() { }
+
+    public TestPlugin(JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
+        super(loader, description, dataFolder, file);
+    }
+}
